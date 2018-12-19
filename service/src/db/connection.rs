7c3a2d52a053ac3d9c34cbbc646b66f1752efb93
Christopher Altona <christopher.altona@adp.com>
2018-12-19 09:04:56 -0500

    Handle errors at the DB level (correctly? lol who knows)

--- service/src/db/connection.rs
+++ service/src/db/connection.rs
@@ -18,12 +18,25 @@ impl DbPool {
     pub fn in_transaction<F, T>(&self, f: F) -> Result<T, String>
         where F: FnOnce(&DbConnection) -> Result<T, String>
     {
-        let connection = self.pool.get().unwrap();
-        connection.transaction::<Result<T, String>, diesel::result::Error, _>(|| {
-            match f(&connection) {
-                Ok(result) => Ok(Ok(result)),
+        match self.pool.get() {
+            Ok(connection) => self.in_transaction_internal(connection, f),
+            Err(error) => Err(format!("{}", error.to_string()))
+        }
+    }
+
+    fn in_transaction_internal<F, T>(&self, connection: DbConnection, f: F) -> Result<T, String>
+        where F: FnOnce(&DbConnection) -> Result<T, String>
+    {
+        let mut result = Err("Result Never Ran".to_string());
+        match connection.transaction(|| {
+            result = f(&connection);
+            match &result {
+                Ok(_) => Ok("Success".to_string()),
                 Err(_) => Err(diesel::result::Error::RollbackTransaction)
             }
-        }).unwrap()
+        }) {
+            Ok(_) => result,
+            Err(error) => Err(format!("{}", error.to_string()))
+        }
     }
 }
