e38c25a842ab44b91587369ee89af7141442d7cd
Christopher Altona <christopher.altona@adp.com>
2019-01-25 08:58:54 -0500

    SSL Bodge

---
+++ infra/cert.sh
@@ -0,0 +1,11 @@
+// Create the keystore post renewal
+openssl pkcs12 -export -in fullchain.pem \ 
+                 -inkey privkey.pem \ 
+                 -out keystore.p12 
+                 -name tomcat \
+                 -CAfile chain.pem \
+                 -caname root
+
+// Allow ssl users to read certificates
+chgrp -R ssl /etc/letsencrypt
+chmod -R g=rX /etc/letsencrypt
--- infra/up.sh
+++ infra/up.sh
@@ -15,6 +15,10 @@ cd -
 
 java -jar ./my-dashboard/service/build/libs/my-dashboard-0.1.0.jar \
         --server.port="${port}" \
+        --security.require-ssl=true \
+        --server.ssl.key-store=/etc/letsencrypt/live/li1044-67.members.linode.com
+        --server.ssl.key-store-type=PKCS12
+        --server.ssl.key-alias=tomcat
         --spring.datasource.url="jdbc:postgresql://localhost:5432/${branch}" \
         --spring.datasource.username=app \
         --spring.datasource.password=password >> "running/log_${branch}.txt" &
