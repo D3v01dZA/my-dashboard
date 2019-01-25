82fb4811ad264cf5f9338053ff106220261e69be
Christopher Altona <christopher.altona@adp.com>
2019-01-25 09:42:35 -0500

    Parameterize slightly

--- infra/up.sh
+++ infra/up.sh
@@ -3,9 +3,6 @@
 branch="${1}"
 port="${2}"
 
-echo "Branch ${branch}"
-echo "Port ${port}"
-
 cd my-dashboard/service
 git fetch
 git checkout "${branch}"
@@ -13,13 +10,18 @@ git pull
 ./gradlew build
 cd -
 
+read db_user < db_user.txt
+read db_pass < db_pass.txt
+read store_pass < store_pass.txt
+
 java -jar ./my-dashboard/service/build/libs/my-dashboard-0.1.0.jar \
         --server.port="${port}" \
         --security.require-ssl=true \
-        --server.ssl.key-store=/etc/letsencrypt/live/li1044-67.members.linode.com \
+        --server.ssl.key-store=keystore \
         --server.ssl.key-store-type=PKCS12 \
+        --server.ssl.key-password="${store_pass}" \
         --server.ssl.key-alias=tomcat \
         --spring.datasource.url="jdbc:postgresql://localhost:5432/${branch}" \
-        --spring.datasource.username=app \
-        --spring.datasource.password=password >> "running/log_${branch}.txt" &
+        --spring.datasource.username="${db_user}" \
+        --spring.datasource.password="${db_pass}" >> "running/log_${branch}.txt" &
 echo $! > "running/pid_${branch}.txt"
