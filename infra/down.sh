264d1c4000ecd363ecec67bda812eb5691692ee8
Christopher Altona <caltona1@gmail.com>
2019-01-28 18:39:02 -0500

    Sanity check inputs and use a properties file

--- infra/down.sh
+++ infra/down.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
-file="running/pid_${1}.txt"
+branch="${1}"
 
+if [[ -z "${branch}" ]]; then
+    echo "No branch specified"
+    exit 1
+fi
+
+file="${branch}/pid.txt"
 echo "Trying to kill process in ${file}"
 
-if [ -f "${file}" ]; then
+if [[ -f "${file}" ]]; then
         echo "Was running"
-        read pid < $file
-        if [ -e "/proc/${pid}" ]; then
+        read pid < ${file}
+        if [[ -e "/proc/${pid}" ]]; then
                 echo "Killing ${pid}"
                 kill "${pid}"
         else
                 echo "Not currently running"
         fi
-        while [ -e "/proc/${pid}" ]; do
+        while [[ -e "/proc/${pid}" ]]; do
                 echo "Waiting for ${pid} to die"
                 sleep 0.1;
         done
--- infra/up.sh
+++ infra/up.sh
@@ -3,6 +3,15 @@
 branch="${1}"
 port="${2}"
 
+if [[ -z "${branch}" ]]; then
+    echo "No branch specified"
+    exit 1
+fi
+if [[ -z "${port}" ]]; then
+    echo "No port specified"
+    exit 1
+fi
+
 cd my-dashboard/service
 git fetch
 git checkout "${branch}"
@@ -14,14 +23,5 @@ read db_user < props/db_user.txt
 read db_pass < props/db_pass.txt
 read store_pass < props/store_pass.txt
 
-java -jar ./my-dashboard/service/build/libs/my-dashboard-0.1.0.jar \
-        --server.port="${port}" \
-        --security.require-ssl=true \
-        --server.ssl.key-store=props/keystore \
-        --server.ssl.key-store-type=PKCS12 \
-        --server.ssl.key-store-password="${store_pass}" \
-        --server.ssl.key-alias=tomcat \
-        --spring.datasource.url="jdbc:postgresql://localhost:5432/${branch}" \
-        --spring.datasource.username="${db_user}" \
-        --spring.datasource.password="${db_pass}" >> "running/log_${branch}.txt" &
-echo $! > "running/pid_${branch}.txt"
+java -jar ./my-dashboard/service/build/libs/my-dashboard-0.1.0.jar --spring.config.additional-location="${branch}/application.properties" >> "${branch}/log.txt" &
+echo $! > "${branch}/pid.txt"
