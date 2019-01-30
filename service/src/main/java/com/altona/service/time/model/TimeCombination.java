7d15b2ca95667c898dec44b889f8867fc8b1d581
Christopher Altona <caltona1@gmail.com>
2019-01-29 19:29:35 -0500

    Rename some fields

--- service/src/main/java/com/altona/service/time/model/TimeCombination.java
+++ service/src/main/java/com/altona/service/time/model/TimeCombination.java
@@ -26,30 +26,30 @@ public class TimeCombination {
         ImmutableRangeMap<Instant, TimeCombination> map = builder.build();
         for (Time time : times) {
             if (time.getType() == TimeType.BREAK) {
-                map.get(time.getStart()).breakTimes.add(time);
+                map.get(time.getStart()).breaks.add(time);
             }
         }
         return new ArrayList<>(map.asMapOfRanges().values());
     }
 
-    private Time workTime;
-    private List<Time> breakTimes;
+    private Time work;
+    private List<Time> breaks;
 
     public boolean isStopped() {
-        return workTime.getEnd().isPresent();
+        return work.getEnd().isPresent();
     }
 
     public Instant getStart() {
-        return workTime.getStart();
+        return work.getStart();
     }
 
     public Instant getEnd(Instant now) {
-        return workTime.getEnd().orElse(now);
+        return work.getEnd().orElse(now);
     }
 
     public LocalTime time(Instant now) {
-        LocalTime time = workTime.time(now);
-        for (Time breakTime: breakTimes) {
+        LocalTime time = work.time(now);
+        for (Time breakTime: breaks) {
             time = time.minus(ChronoUnit.NANOS.between(breakTime.getStart(), breakTime.getEnd().orElse(now)), ChronoUnit.NANOS);
         }
         return time;
