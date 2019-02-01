60a49cfd403ae8b6b64f642a3ce4bcb85729b771
Christopher Altona <christopher.altona@adp.com>
2019-02-01 09:16:01 -0500

    Cave and write some unit tests so I don't lose my sanity

--- service/src/main/java/com/altona/util/LocalDateIterator.java
+++ service/src/main/java/com/altona/util/LocalDateIterator.java
@@ -4,6 +4,7 @@ import org.springframework.util.Assert;
 
 import java.time.LocalDate;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 public class LocalDateIterator implements Iterator<LocalDate> {
 
@@ -27,6 +28,9 @@ public class LocalDateIterator implements Iterator<LocalDate> {
 
     @Override
     public LocalDate next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
         LocalDate current = this.current;
         this.current = this.current.plusDays(1);
         return current;
---
+++ service/src/test/java/com/altona/util/LocalDateIteratorTest.java
@@ -0,0 +1,43 @@
+package com.altona.util;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LocalDateIteratorTest {
+
+    @Test
+    public void testInvalid() {
+        assertThrows(IllegalArgumentException.class, () -> LocalDateIterator.inclusive(LocalDate.of(2018, 1, 1), LocalDate.of(2017, 1, 1)).iterator());
+    }
+
+    @Test
+    public void testInclusive() {
+        Iterable<LocalDate> iterable = LocalDateIterator.inclusive(LocalDate.of(2018, 1, 1), LocalDate.of(2018, 1, 3));
+
+        Iterator<LocalDate> firstIterator = iterable.iterator();
+        assertTrue(firstIterator.hasNext());
+        assertEquals(LocalDate.of(2018, 1, 1), firstIterator.next());
+        assertTrue(firstIterator.hasNext());
+        assertEquals(LocalDate.of(2018, 1, 2), firstIterator.next());
+        assertTrue(firstIterator.hasNext());
+        assertEquals(LocalDate.of(2018, 1, 3), firstIterator.next());
+        assertFalse(firstIterator.hasNext());
+        assertThrows(NoSuchElementException.class, firstIterator::next);
+
+        Iterator<LocalDate> secondIterator = iterable.iterator();
+        assertTrue(secondIterator.hasNext());
+        assertEquals(LocalDate.of(2018, 1, 1), secondIterator.next());
+        assertTrue(secondIterator.hasNext());
+        assertEquals(LocalDate.of(2018, 1, 2), secondIterator.next());
+        assertTrue(secondIterator.hasNext());
+        assertEquals(LocalDate.of(2018, 1, 3), secondIterator.next());
+        assertFalse(secondIterator.hasNext());
+        assertThrows(NoSuchElementException.class, secondIterator::next);
+    }
+
+}
