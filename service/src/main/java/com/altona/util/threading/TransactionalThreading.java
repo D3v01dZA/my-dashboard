a7fe200e141aa454a05b33e2d9be23b6568e8a0b
Christopher Altona <christopher.altona@adp.com>
2019-06-13 16:31:31 -0400

    Make at most 10 threads

--- service/src/main/java/com/altona/util/threading/TransactionalThreading.java
+++ service/src/main/java/com/altona/util/threading/TransactionalThreading.java
@@ -1,6 +1,7 @@
 package com.altona.util.threading;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,11 +16,12 @@ import java.util.stream.Collectors;
 public class TransactionalThreading {
 
     private TransactionalExecutor transactionalExecutor;
-    private ExecutorService executorService = Executors.newCachedThreadPool();
+    private ExecutorService executorService;
 
     @Autowired
-    public TransactionalThreading(TransactionalExecutor transactionalExecutor) {
+    public TransactionalThreading(TransactionalExecutor transactionalExecutor, @Value("${background.threads}") int threads) {
         this.transactionalExecutor = transactionalExecutor;
+        this.executorService = Executors.newFixedThreadPool(threads);
     }
 
     public <T> List<T> executeInReadOnlyTransaction(List<Supplier<T>> suppliers) {
