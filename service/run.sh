aca87613d57ac3e87cb30970e74d8307919f56b3
Christopher Altona <christopher.altona@adp.com>
2020-10-23 18:11:26 -0400

    Add docker specific

--- service/run.sh
+++ service/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -jar "./build/libs/my-dashboard-0.1.0.jar" --spring.config.additional-location="./config/application.properties" --logging.file="./logs/log.txt"
+java -jar "./build/libs/my-dashboard-0.1.0.jar" --spring.config.additional-location="./config/application-docker.properties" --logging.file="./logs/log.txt"
